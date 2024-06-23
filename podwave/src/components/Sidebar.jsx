b21235f844e6a8327cc1bd343bbbf7c0212171b3
Clinton_Mabunda <mclintonsa@gmail.com>
2024-06-23 12:13:11 +0200

    Amended closeRounded text

--- podwave/src/components/Sidebar.jsx
+++ podwave/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import {HomeRounded, CloseRounded} from "@mui/icons-material"
+import {HomeRounded, closeRounded} from "@mui/icons-material"
 
 const MenuContainer = styled.div``;
 const Logo = styled.div``;
