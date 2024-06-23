3af55cfc0cb4ca567c77f34be728f47e76ff1d82
Clinton_Mabunda <mclintonsa@gmail.com>
2024-06-23 11:50:18 +0200

    Imported themes to App.js file

--- podwave/src/App.js
+++ podwave/src/App.js
@@ -1,8 +1,24 @@
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "./utils/Themes";
+
+const Container = styled.div`
+  background: ${({theme})=>theme.bg};
+  width: 100%;
+  height: 100vh; 
+`;
+
 function App() {
+  //hooks
+  const [darkMode, SetDarkMode] = useState(true);
+
+
   return (
-    <div>
+    <ThemeProvider theme={darkTheme}>
+     <Container>
       PodWave
-    </div>
+     </Container>
+    </ThemeProvider>
   );
 }
 
